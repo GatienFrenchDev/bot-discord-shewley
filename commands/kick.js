8a5ea496104b3beb9a6e867fc16c8c0075ea190b
Gatien <80203026+GatienFrenchDev@users.noreply.github.com>
2021-06-01 22:01:41 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'kick',
     description: 'kick un utilisateur du serveur !',
     execute(message, args) {
-        if(!message.member.hasPermission('KICK_MEMBERS'))) return message.reply ("`Vous n'avez pas les permissions pour exécuter cette commande !`")
+        if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply ("`Vous n'avez pas les permissions pour exécuter cette commande !`")
         if (!args[0]) return message.reply ("`Tu dois identifier la personne à kick !`");
         if (!message.mentions.members.first()) return message.reply ("`Tu dois identifier la personne a kick !`");
         else{
